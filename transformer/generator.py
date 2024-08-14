92c7b86921b2f031b776ee5a77fa6fb9e8f17802
André Costa <andrehesilva@gmail.com>
2024-08-13 22:28:57 -0300

    Add code from annotated transformer

---
+++ transformer/generator.py
@@ -0,0 +1,12 @@
+from torch.nn.functional import log_softmax
+import torch.nn as nn
+
+class Generator(nn.Module):
+    "Define standard linear + softmax generation step."
+
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        return log_softmax(self.proj(x), dim=-1)
---
+++ transformer/model.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+
+from transformer.mha import MultiHeadedAttention
+from transformer.ffn import PositionwiseFeedForward
+from transformer.penc import PositionalEncoding
+from transformer.encdenc import EncoderDecoder
+from transformer.encoder import Encoder, EncoderLayer
+from transformer.decoder import Decoder, DecoderLayer
+from transformer.embed import Embeddings
+from transformer.generator import Generator
+
+
+import copy
+
+def make_model(
+    src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1
+):
+    "Helper: Construct a model from hyperparameters."
+    c = copy.deepcopy
+    attn = MultiHeadedAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab),
+    )
+
+    # This was important from their code.
+    # Initialize parameters with Glorot / fan_avg.
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
